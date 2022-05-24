2ecf789b31d734ad0f71719d7b397b8ee86687cb
JonDonCodes <105407755+JonDonCodes@users.noreply.github.com>
2022-05-23 22:48:30 -0400

    Add files via upload

--- NumberGame.js
+++ NumberGame.js
@@ -1,180 +1,250 @@
+// startGame()
+let num_of_games_played;
+let num_of_games_completed;
+let average_time;
+let last_time_to_finish;
+let lastGameDate;
+let lastGameCompleted;
 
-let target = document.querySelector("#targetVar");
-let countdown = document.querySelector("#countdownVar");
-let numInputs = 0;
-let opInputs = 0;
-let enabledButtonType = "Num" // "Op"
-let numTiles = document.querySelectorAll(".NumTile");
-let opTiles = document.querySelectorAll(".OpTile");
-let formulaNumTiles = document.querySelectorAll(".formulaTile");
-let formulaOpTiles = document.querySelectorAll(".formulaTileOP");
-let backBtn = document.querySelector("#BackBtn");
-let enterBtn = document.querySelector("#EnterBtn");
-var interval = null;
-let targetResult = getRandomInt(20,120);
-let finalContainer = document.querySelector(".FinalScore");
+const date            = new Date();
+const day             = date.getDate();
+const month           = date.getMonth()+1;
+const year            = date.getFullYear();
+const sDate           = `${month}.${day}${year}`;
+// const sDate = "2.292022"
 
-countdown.innerHTML = 60;
+let seed              = parseFloat(sDate)*100000000;
+let target            = document.querySelector("#targetVar");
+let timer             = document.querySelector("#timerVar");
+let numInputs         = 0;
+let opInputs          = 0;
+let enabledButtonType = "Num"; // "Op"
+let numTiles          = document.querySelectorAll(".NumTile");
+let opTiles           = document.querySelectorAll(".OpTile");
+let formulaNumTiles   = document.querySelectorAll(".formulaTile");
+let formulaOpTiles    = document.querySelectorAll(".formulaTileOP");
+let backBtn           = document.querySelector("#BackBtn");
+let enterBtn          = document.querySelector("#EnterBtn");
+let targetResult      = getPseudoRandomInt(seed,20,2000);
+let finalContainer    = document.querySelector(".FinalScore");
+let playButton        = document.querySelector(".PlayButton");
+let popupButton       = document.querySelector("#Popup-button");
+let game              = document.querySelector(".GameContainer");
+let popup             = document.querySelector(".Popup");
 
-controlOpTiles(0);
+// localStorage.clear();
 
-interval = setInterval(decrementTimer,1000);
+popup.style.display = "none";
 
-let lst = print_factors(targetResult,[])
-while (lst.length === 2) {
-	targetResult = getRandomInt(20,120);
-	lst = print_factors(targetResult,[])
-}
+if (targetResult > 1500) {
+	targetResult = targetResult - 600;
+} else if (targetResult > 1000) {
+	targetResult = targetResult - 500;
+} 
 
-target.innerHTML = targetResult;
-
-const lst_ran_index = getRandomInt(1,lst.length+1);
-// const choice1 = lst[lst_ran_index-1];
-// const choice2 = targetResult/choice1;
-const [choice1,choice2] = perform_rand_op(targetResult,getRandomInt(0,4),0);
-console.log(choice1);
-console.log(choice2);
-if (choice1 > choice2) {
-    var arr1 = perform_rand_op(choice1,getRandomInt(0,4));
-    var arr2 = perform_rand_op(choice2,getRandomInt(0,3));
+// Local Storage
+if (localStorage.getItem('num_of_games_played') == null) {
+	populateStorage();
 } else {
-    var arr1 = perform_rand_op(choice1,getRandomInt(0,3));
-    var arr2 = perform_rand_op(choice2,getRandomInt(0,4));
+	num_of_games_played    = localStorage.getItem('num_of_games_played');
+	num_of_games_completed = localStorage.getItem('num_of_games_completed');
+	average_time           = localStorage.getItem('average_time');
+	last_time_to_finish    = localStorage.getItem('last_time_to_finish');
+	lastGameDate           = localStorage.getItem('lastGameDate');
+	lastGameCompleted      = localStorage.getItem('lastGameCompleted');
 }
 
-choices = arr1.concat(arr2);
-choices = shuffle(choices);
+function populateStorage() {
+	localStorage.setItem('num_of_games_played',0);
+	localStorage.setItem('num_of_games_completed',0);
+	localStorage.setItem('average_time',0);
+	localStorage.setItem('last_time_to_finish',0);
+	localStorage.setItem('lastGameDate',0);
+	localStorage.setItem('lastGameCompleted',0);
 
-numTiles[0].innerHTML = choices[0];
-numTiles[1].innerHTML = choices[1];
-numTiles[2].innerHTML = choices[2];
-numTiles[3].innerHTML = choices[3];
+	num_of_games_played    = localStorage.getItem('num_of_games_played');
+	num_of_games_completed = localStorage.getItem('num_of_games_completed');
+	average_time           = localStorage.getItem('average_time');
+	last_time_to_finish    = localStorage.getItem('last_time_to_finish');
+	lastGameDate           = localStorage.getItem('lastGameDate');
+	lastGameCompleted      = localStorage.getItem('lastGameCompleted');
+}
 
-for (let i = 0; i < 4; i++) {
-	numTiles[i].addEventListener("click", function() {
-		formulaNumTiles[numInputs].innerHTML = numTiles[i].innerHTML;
-		numInputs++;
-		controlOpTiles(1);
-	});
-	opTiles[i].addEventListener("click", function() {
-		formulaOpTiles[opInputs].innerHTML = opTiles[i].innerHTML;
-		formulaOpTiles[opInputs].style.border = "none";
-		opInputs++;
-		controlOpTiles(0);
-	});
-};
+function populateStats() {
+	document.querySelector("#num_of_games_played").innerHTML    = num_of_games_played;
+	document.querySelector("#num_of_games_completed").innerHTML = num_of_games_completed;
+	document.querySelector("#average_time").innerHTML           = average_time;
+	document.querySelector("#last_time_to_finish").innerHTML    = last_time_to_finish;
+}
 
-backBtn.addEventListener("click", function() {
-	if ( numInputs != 0 || opInputs != 0) {
-		if (enabledButtonType==="Num") {
-			opInputs--;
-			formulaOpTiles[opInputs].innerHTML = "";
-			formulaOpTiles[opInputs].style.border = "solid";
-			formulaOpTiles[opInputs].style.border = "solid 2px";
-			controlOpTiles(1);
+game.style.visibility = 'hidden';
+playButton.addEventListener("click", function() {
+
+	if (lastGameDate != sDate) {
+		lastGameDate = sDate;
+		localStorage.setItem('lastGameDate',sDate);
+		num_of_games_played++;
+		localStorage.setItem('num_of_games_played',num_of_games_played);
+		localStorage.setItem('lastGameCompleted',0);
+		game.style.visibility = 'visible';
+		startGame();
+	} else {
+		if (localStorage.getItem('lastGameCompleted') != 1) {
+
 		} else {
-			numInputs--;
-			formulaNumTiles[numInputs].innerHTML = "";
-			controlOpTiles(0);
+			popup.style.display = "flex";
+		  game.style.visibility = 'hidden';
+		  playButton.style.display = "none";
+		  populateStats();
 		}
 	}
 });
 
-enterBtn.addEventListener("click", function() {
-	if ( numInputs ==4 ) {
-		let result1 = performOp(formulaNumTiles[0].innerHTML,formulaNumTiles[1].innerHTML,formulaOpTiles[0].innerHTML);
-		let result2 = performOp(formulaNumTiles[2].innerHTML,formulaNumTiles[3].innerHTML,formulaOpTiles[2].innerHTML);
-		let finalResult = performOp(result1,result2,formulaOpTiles[1].innerHTML);
-		if (finalResult != targetResult) {
-			finalContainer.innerHTML = "Nope, Keep Trying"
-		} else {
-			finalContainer.innerHTML = "You got it!"
-			disableTimer();
-		}
-	}	
+popupButton.addEventListener("click", function() {
+	popup.style.display = "none";
+	playButton.style.display = "block";
 });
 
+// testTargetDistribution()
 
-// Functions
+function startGame() {
+	playButton.style.display = "none";
+	timer.innerHTML = "00:00";
+	controlOpTiles(0);
 
-function controlOpTiles(enable) {
-	for (let i=0;i<4;i++) {
-		if (enable === 1) {
-			opTiles[i].disabled = false;
-			numTiles[i].disabled = true;
-			enabledButtonType = "Op"
-			numTiles[i].style.opacity = "0.4"
-			opTiles[i].style.opacity = "1"
-		} else if (enable===0) {
+	let lst = print_factors(targetResult,[])
+	while (lst.length === 2) {
+		targetResult = targetResult + 1;
+		lst = print_factors(targetResult,[])
+	}
+
+	target.innerHTML = targetResult;
+
+	let startTime = Date.now();
+	let interval = setInterval(function printTime() {
+		let elapsedTime = Date.now() - startTime;
+		timer.innerHTML = timeToString(elapsedTime);
+	}, 1000)
+
+	const [choice1,choice2] = perform_rand_op(targetResult,getPseudoRandomOp(seed),0);
+	if (choice1 > choice2) {
+	    var arr1 = perform_rand_op(choice1,getPseudoRandomInt(seed,0,4));
+	    var arr2 = perform_rand_op(choice2,getPseudoRandomOp(seed));
+	} else {
+	    var arr1 = perform_rand_op(choice1,getPseudoRandomOp(seed));
+	    var arr2 = perform_rand_op(choice2,getPseudoRandomInt(seed,0,4));
+	}
+
+	choices = arr1.concat(arr2);
+	choices = shuffle(choices);
+
+	numTiles[0].innerHTML = choices[0];
+	numTiles[1].innerHTML = choices[1];
+	numTiles[2].innerHTML = choices[2];
+	numTiles[3].innerHTML = choices[3];
+
+	for (let i = 0; i < 4; i++) {
+		numTiles[i].addEventListener("click", function() {
+			formulaNumTiles[numInputs].innerHTML = numTiles[i].innerHTML;
+			numInputs++;
+			controlOpTiles(1);
+		});
+		opTiles[i].addEventListener("click", function() {
+			formulaOpTiles[opInputs].innerHTML = opTiles[i].innerHTML;
+			formulaOpTiles[opInputs].style.border = "none";
+			opInputs++;
+			controlOpTiles(0);
+		});
+	};
+
+	backBtn.addEventListener("click", function() {
+		if ( numInputs != 0 || opInputs != 0) {
+			if (enabledButtonType==="Num") {
+				opInputs--;
+				formulaOpTiles[opInputs].innerHTML = "";
+				formulaOpTiles[opInputs].style.border = "solid";
+				formulaOpTiles[opInputs].style.border = "solid 2px";
+				controlOpTiles(1);
+			} else {
+				numInputs--;
+				formulaNumTiles[numInputs].innerHTML = "";
+				controlOpTiles(0);
+			}
+		}
+	});
+
+	enterBtn.addEventListener("click", function() {
+		if ( numInputs ==4 ) {
+			let result1 = performOp(formulaNumTiles[0].innerHTML,formulaNumTiles[1].innerHTML,formulaOpTiles[0].innerHTML);
+			let result2 = performOp(formulaNumTiles[2].innerHTML,formulaNumTiles[3].innerHTML,formulaOpTiles[2].innerHTML);
+			let finalResult = performOp(result1,result2,formulaOpTiles[1].innerHTML);
+			if (finalResult != targetResult) {
+				finalContainer.innerHTML = "Nope, Keep Trying"
+			} else {
+				finalContainer.innerHTML = "You got it!"
+				disableTimer();
+				localStorage.setItem('lastGameCompleted',1);
+				  
+				let completed_in_seconds = timeStringToSeconds(last_time_to_finish);
+				let avg_t = ((average_time*num_of_games_completed)+completed_in_seconds)/(num_of_games_completed+1);
+				average_time = secondsToString(avg_t);
+				num_of_games_completed++;
+				localStorage.setItem('num_of_games_completed',num_of_games_completed);
+				localStorage.setItem('average_time',average_time);
+		    localStorage.setItem('last_time_to_finish',last_time_to_finish);
+
+		    popup.style.display = "flex";
+		    game.style.visibility = 'hidden';
+		    populateStats();
+			}
+		}	
+	});
+
+
+	// Game Functions
+
+	function controlOpTiles(enable) {
+		for (let i=0;i<4;i++) {
+			if (enable === 1) {
+				opTiles[i].disabled = false;
+				numTiles[i].disabled = true;
+				enabledButtonType = "Op"
+				numTiles[i].style.opacity = "0.4"
+				opTiles[i].style.opacity = "1"
+			} else if (enable===0) {
+				opTiles[i].disabled = true;
+				numTiles[i].disabled = false;
+				enabledButtonType = "Num"
+				numTiles[i].style.opacity = "1"
+				opTiles[i].style.opacity = "0.4"
+			}
+			
+		}
+	};
+
+	function disableAllTiles() {
+		for (let i=0;i<4;i++) {
 			opTiles[i].disabled = true;
-			numTiles[i].disabled = false;
-			enabledButtonType = "Num"
-			numTiles[i].style.opacity = "1"
+			numTiles[i].disabled = true;
+			numTiles[i].style.opacity = "0.4"
 			opTiles[i].style.opacity = "0.4"
 		}
-		
 	}
-};
 
-function disableAllTiles() {
-	for (let i=0;i<4;i++) {
-		opTiles[i].disabled = true;
-		numTiles[i].disabled = true;
-		numTiles[i].style.opacity = "0.4"
-		opTiles[i].style.opacity = "0.4"
-	}
-}
+	function disableTimer() {
+		clearInterval(interval);
+		last_time_to_finish = timer.innerHTML;
+	};
 
-function disableTimer() {
-	clearInterval(interval);
-};
-
-function decrementTimer() {
-	const x = countdown.innerHTML;
-	countdown.innerHTML = x-1;
-	if (countdown.innerHTML == 0) {
-		finalContainer.innerHTML = "Time's Up!"
-		disableAllTiles()
-		disableTimer()
-	}
-};
-
-function getRandomInt(min,max) {
-	min = Math.ceil(min);
- 	max = Math.floor(max);
- 	return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-};
-
-function print_factors(mainNum,lst=[]) {
-	console.log("The factors of "+mainNum+" are:")
-	for (let i=1;i<=mainNum;i++) {
-		if (mainNum % i == 0) {
-			// console.log(i);
-			lst.push(i);
-		}
-	}
-	return lst;
-};
-
-function perform_rand_op(num,op,difficulty=0) {
-	// let modifier = getRandomInt(2,10);
-	let modifier = getModifier(difficulty);
-	let result = 0;
-	if (op == 0) {
-		result = num + modifier;
-	} else if (op == 1) {
-		result = num - modifier;
-	} else if (op == 2) {
-		result = num * modifier;
-	} else if (op == 3) {
-		result = num / modifier;
-		while (Number.isInteger(result)===false) {
-			// let modifier = getRandomInt(2,10);
-			modifier = getModifier(difficulty);
-			result = num / modifier;
-		}
-	}
-	return [result,modifier];
+	// function decrementTimer() {
+	// 	const x = timer.innerHTML;
+	// 	timer.innerHTML = x-1;
+	// 	if (timer.innerHTML == 0) {
+	// 		finalContainer.innerHTML = "Time's Up!"
+	// 		disableAllTiles()
+	// 		disableTimer()
+	// 	}
+	// };
 }
 
 function performOp(num1,num2,op) {
@@ -193,34 +263,146 @@ function performOp(num1,num2,op) {
 	return result;
 }
 
+function getRandomInt(min,max) {
+	min = Math.ceil(min);
+ 	max = Math.floor(max);
+ 	return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+};
+
+function print_factors(mainNum,lst=[]) {
+	// console.log("The factors of "+mainNum+" are:")
+	for (let i=1;i<=mainNum;i++) {
+		if (mainNum % i == 0) {
+			// console.log(i);
+			lst.push(i);
+		}
+	}
+	return lst;
+};
+
+function perform_rand_op(num,op,difficulty=0) {
+	let modifier = getModifier(difficulty);
+	let result = 0;
+	if (op == 0) {
+		result = num + modifier;
+	} else if (op == 1) {
+		result = num - modifier;
+	} else if (op == 2) {
+		// Modifier can't be too high multiplying #s
+		// so get an easier modifier
+		modifier = getModifier(-1);
+		result = num * modifier;
+	} else if (op == 3) {
+		result = num / modifier;
+		while (Number.isInteger(result)===false) {
+			modifier = modifier + 1;
+			result = num / modifier;
+		}
+	}
+	return [result,modifier];
+}
+
 function getModifier(difficulty) {
 	if (difficulty === 0) {
-		modifier = getRandomInt(2,10);
+		modifier = getPseudoRandomInt(seed,2,10);
 	} else if (difficulty === 1) {
-		modifier = getRandomInt(2,20);
+		modifier = getPseudoRandomInt(seed,2,20);
 	} else if (difficulty === 2) {
-		modifier = getRandomInt(9,20);
+		modifier = getPseudoRandomInt(seed,9,20);
+	}	else if (difficulty === -1) {
+		modifier = getPseudoRandomOp(seed)+1;
 	} else {
-		modifier = getRandomInt(2,30);
+		modifier = getPseudoRandomInt(seed,2,30);
 	}
 	return modifier;
 }
 
+function getPseudoRandomInt(seed,min=1,max=100) {
+	const prn = seed * 16807 * day % 2147483647
+	let result = prn;
+
+  // max is exclusive
+	while (result > max) {
+		result = result / 2;
+	};
+
+	result = Math.floor(result);
+
+	while (result < min) {
+		result = result + (max-min-1)
+	}
+
+	return result;
+};
+
+// Pseudo random op based on day of month
+function getPseudoRandomOp(seed,max=4) {
+  return day % max;
+}
+
 function shuffle(array) {
   let currentIndex = array.length,  randomIndex;
-
-  // While there remain elements to shuffle.
+	  // While there remain elements to shuffle.
   while (currentIndex != 0) {
-
-    // Pick a remaining element.
+	  // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
-
-    // And swap it with the current element.
+	  // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
       array[randomIndex], array[currentIndex]];
   }
-
   return array;
 }
 
+function timeToString(time) {
+  let diffInHrs = time / 3600000;
+  let hh = Math.floor(diffInHrs);
+	let diffInMin = (diffInHrs - hh) * 60;
+  let mm = Math.floor(diffInMin);
+	let diffInSec = (diffInMin - mm) * 60;
+  let ss = Math.floor(diffInSec);
+	let formattedMM = mm.toString().padStart(2, "0");
+  let formattedSS = ss.toString().padStart(2, "0");
+	return `${formattedMM}:${formattedSS}`;
+}
+
+function timeStringToSeconds(timeString) {
+	const minutes = timeString.slice(0,2);
+	const seconds = timeString.slice(3,5);
+
+	return minutes * 60 + seconds;
+}
+
+function secondsToString(seconds) {
+	let mm = seconds/60;
+	mm = Math.floor(mm);
+	let ss = seconds % 60;
+	let formattedMM = mm.toString().padStart(2, "0");
+  let formattedSS = ss.toString().padStart(2, "0");
+  return `${formattedMM}:${formattedSS}`;
+}
+
+// Test distribution of targetResult
+// Ideally you get as close to 360 #s as possible for each day
+function testTargetDistribution() {
+	let tlst = [];
+	for (let i = 1;i<13;i++) {
+		for (let j = 1;j<31;j++) {
+			tdate = `${i}.${j}2022`
+			tseed = parseFloat(tdate)*100000000;
+			tres = getPseudoRandomInt(tseed,20,2000);
+			if (tres > 1500) {
+				tres = tres - 600;
+			} else if (tres > 1000) {
+				tres = tres - 500;
+			} 
+			tlst.push(tres)
+		}
+	}
+
+	let ulst = tlst.reduce(function(a,b){if(a.indexOf(b)<0)a.push(b);return a;},[]);
+
+	console.log("TestListLength " + tlst.length)
+	console.log("UniqListLength " + ulst.length)
+	console.log("UniqList " + ulst)
+}
